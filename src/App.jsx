08257e237a4e3395099c07676131fd70436794ec
ygstudio-game <yadnyeshsunilborole@gmail.com>
2025-12-25 12:19:39 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import LandingPage from "./components/pages/Landing/Landingpage.jsx";
+import LandingPage from "./components/pages/Landing/LandingPage.jsx";
 import TerminalMode from "./components/pages/TerminalPage/TerminalMode.jsx";
 import ModernPage from "./components/pages/ModernPage/ModernPage.jsx";
 import ScrollToTop from "./ScrollToTop";
